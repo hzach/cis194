ff4ae43ee7440a0dfebedf26720dc831d6141e33
ztidwell <ztidwell@planview.com>
2016-07-22 19:44:13 -0500

    import Data.Char, fix indention error, reverse isInt

--- hw2/LogAnalysis.hs
+++ hw2/LogAnalysis.hs
@@ -1,7 +1,23 @@
-{-# GHC_OPTIONS -Wall #-}
+{-# OPTIONS_GHC -Wall #-}
 
 module LogAnalysis where
   import Log
+  import Data.Char
+
+  -- Utils ----
+  isInt :: String -> Bool
+  isInt s =
+    case dropWhile isDigit s of
+      "" -> True
+      _  -> False
 
   parseMessage :: String -> LogMessage
-  parseMessage
+  parseMessage msg =
+    case words msg of
+      ("E":n:t:xs)
+        | isInt n && isInt t -> LogMessage (Error $ read n) (read t) (unwords xs)
+      ("W":t:xs)
+        | isInt t            -> LogMessage Warning (read t) (unwords xs)
+      ("I":t:xs)
+        | isInt t            -> LogMessage Info (read t) (unwords xs)
+      _                      -> Unknown ("Format error: " ++ msg)

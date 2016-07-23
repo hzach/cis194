03e1aea2cbbb9782829b70818f66f5753f9e8cf8
ztidwell <ztidwell@planview.com>
2016-07-23 00:41:52 -0500

    move stuff around, add insert function

--- hw2/src/Log.hs
+++ hw2/src/Log.hs
@@ -19,6 +19,10 @@ data MessageTree = Leaf
                  | Node MessageTree LogMessage MessageTree
   deriving (Show, Eq)
 
+instance Ord LogMessage where
+  compare (LogMessage _ t1 _) (LogMessage _ t2 _) = t1 `compare` t2
+  compare _ _ = error "comparison failed!"
+
 -- | @testParse p n f@ tests the log file parser @p@ by running it
 --   on the first @n@ lines of file @f@.
 testParse :: (String -> [LogMessage])
--- hw2/src/LogAnalysis.hs
+++ hw2/src/LogAnalysis.hs
@@ -1,10 +1,9 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module LogAnalysis where
-  import Log
   import Data.Char
+  import Log
 
-  -- Utils ----
   isInt :: String -> Bool
   isInt s =
     case dropWhile isDigit s of
@@ -14,10 +13,18 @@ module LogAnalysis where
   parseMessage :: String -> LogMessage
   parseMessage msg =
     case words msg of
-      ("E":n:t:xs)
+      ("E":n:t:xs) 
         | isInt n && isInt t -> LogMessage (Error $ read n) (read t) (unwords xs)
       ("W":t:xs)
         | isInt t            -> LogMessage Warning (read t) (unwords xs)
       ("I":t:xs)
         | isInt t            -> LogMessage Info (read t) (unwords xs)
       _                      -> Unknown ("Format error: " ++ msg)
+
+  insert :: LogMessage -> MessageTree -> MessageTree
+  insert (Unknown _) tree = tree
+  insert msg Leaf = Node Leaf msg Leaf
+  insert msg (Node left value right) = 
+    case msg `compare` value of
+      LT -> insert msg left
+      _  -> insert msg right

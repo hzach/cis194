8839aebc16df1fec90548a9ff0e2f919b5f1f3fe
ztidwell <ztidwell@planview.com>
2016-07-23 20:18:18 -0500

    complete LogAnalysis

--- hw2/src/LogAnalysis.hs
+++ hw2/src/LogAnalysis.hs
@@ -1,6 +1,7 @@
 {-# OPTIONS_GHC -Wall #-}
 
 module LogAnalysis where
+  import Data.List.Split
   import Data.Char
   import Log
 
@@ -21,10 +22,27 @@ module LogAnalysis where
         | isInt t            -> LogMessage Info (read t) (unwords xs)
       _                      -> Unknown ("Format error: " ++ msg)
 
+  parse :: String -> [LogMessage]
+  parse s = map parseMessage (splitOn "\n" s)
+
   insert :: LogMessage -> MessageTree -> MessageTree
   insert (Unknown _) tree = tree
   insert msg Leaf = Node Leaf msg Leaf
   insert msg (Node left value right) = 
     case msg `compare` value of
-      LT -> insert msg left
-      _  -> insert msg right
+      LT -> Node (insert msg left) value right
+      _  -> Node left value (insert msg right)
+  
+  inOrder :: MessageTree -> [LogMessage]
+  inOrder Leaf = []
+  inOrder (Node left msg right) = (inOrder left) ++ [msg] ++ (inOrder right)
+
+  build :: [LogMessage] -> MessageTree
+  build [] = Leaf
+  build (x:xs) = insert x $ build xs
+
+  whatWentWrong :: [LogMessage] -> [String]
+  whatWentWrong x = map text $ filter isError . inOrder $ build x
+      where isError (LogMessage (Error _) _ _) = True
+            isError _ = False
+            text = \(LogMessage _ _ c) -> c
